0a910d4fc1e310f2fbaedac82d9f574890434d65
Ahihi <luuvietsoan@gmail.com>
2025-03-16 23:12:41 +0700

    Updates

---
+++ precache-manifest.42090c30533fd4ae14a848a85116e2da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1acfd6caada4079ebac741b60689bde",
+    "url": "/qr_code/index.html"
+  },
+  {
+    "revision": "4b89cf9b85515763b1e2",
+    "url": "/qr_code/static/css/2.0037ae8c.chunk.css"
+  },
+  {
+    "revision": "e0eba42acf6a161cedd6",
+    "url": "/qr_code/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b89cf9b85515763b1e2",
+    "url": "/qr_code/static/js/2.a9b22ab5.chunk.js"
+  },
+  {
+    "revision": "e0eba42acf6a161cedd6",
+    "url": "/qr_code/static/js/main.1d646c66.chunk.js"
+  },
+  {
+    "revision": "1f85b36efd413a790a76",
+    "url": "/qr_code/static/js/runtime~main.a767a597.js"
+  }
+]);
